61f1b175cbefc2b32379bb2ef68205c64ed68802
hippo_o_matic <hippo.o.matic@gmail.com>
2018-09-16 18:47:49 -0700

    lua fix

--- src/devices/luafile.cpp
+++ src/devices/luafile.cpp
@@ -3,8 +3,8 @@
 LuaFile::LuaFile(std::string path){
 	L = luaL_newstate(); //Create a new Luastate
 	if (luaL_loadfile(L, path.c_str()) || lua_pcall(L, 0, 0, 0)) {
-		std::cout<<"Error: script not loaded ("<<path<<")"<<std::endl;
-		L = 0;
+		std::cout<<"[!] LUA|6: Lua Error: script not loaded ("<<path<<")"<<std::endl;
+		lua_close(L);
 	}
 }
 LuaFile::~LuaFile(){
@@ -12,18 +12,25 @@ LuaFile::~LuaFile(){
 }
 
 void LuaFile::printError(std::string name, std::string reason) {
-	std::cout<<"[!] LUA|24: Lua Error: couldn't get ["<<(nspace + "." + name)<<"]: "<<reason<<std::endl;
+	std::cout<<"[!] LUA|15: Lua Error: couldn't get ["<<(name)<<"]: "<<reason<<std::endl;
 }
 
 void LuaFile::clean(){
 	lua_pop(L, lua_gettop(L)); //Removes all items from the lua stack
 }
 
+void LuaFile::getNamespace(std::string name){ 
+		if(name !="global")
+			nspace = name + ".";
+		else
+			nspace = "";
+	}
+
 bool LuaFile::gettostack(std::string name){
 	int lev = 0; //Our level in the stack
 	std::string var = "";
-	for (unsigned int i = 0; i < (nspace + "." + name).size(); i++){
-		if((nspace + "." + name).at(i) == '.'){
+	for (unsigned int i = 0; i < (nspace + name).size(); i++){
+		if((nspace + name).at(i) == '.'){
 			if(lev == 0){
 				lua_getglobal(L, var.c_str());
 			} else {
@@ -31,14 +38,14 @@ bool LuaFile::gettostack(std::string name){
 			}
 
 			if (lua_isnil(L, -1)){
-				printError((nspace + "." + name), var + " is not defined");
+				printError((nspace + name), var + " is not defined");
 				return false;
 			} else {
 				var = "";
 				lev++;
 			}
 		} else {
-			var += (nspace + "." + name).at(i);
+			var += (nspace + name).at(i);
 		}
 	}
 
@@ -49,7 +56,7 @@ bool LuaFile::gettostack(std::string name){
 	}
 
 	if(lua_isnil(L, -1)){
-		printError((nspace + "." + name), var + " is not defined");
+		printError((nspace + name), var + " is not defined");
 		return false;
 	}
 
@@ -60,7 +67,21 @@ std::vector<std::string> LuaFile::getKeys(){
 
 }
 
-void LuaFile::parse(std::string name, ...){
+
+void LuaFile::push(bool value){
+	lua_pushboolean(L, value);
+}
+void LuaFile::push(int value){
+	lua_pushinteger(L, value);
+}
+void LuaFile::push(float value){
+	lua_pushnumber(L, value);
+}
+void LuaFile::push(std::string value){
+	lua_pushstring(L, value.c_str());
+}
+
+/*void LuaFile::parse(std::string name, ...){
 	va_list ap;
 	int total = ;
 	va_start(ap, total);
@@ -73,4 +94,4 @@ void LuaFile::parse(std::string name, ...){
 
 
 	clean();
-}
+}*/
---
+++ tests/include/vulkan.h
@@ -0,0 +1,9 @@
+#ifndef HIPPO_VULKAN_H
+#define HIPPO_VULKAN_H
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h> 
+
+
+
+#endif
