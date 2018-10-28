4aefd22fff0a5f1b5d4e427ceded247588327c58
hippo_o_matic <hippo.o.matic@gmail.com>
2018-10-27 21:55:19 -0700

    Moving homemade structures to generic

--- include/luarium/generic.h
+++ include/luarium/generic.h
@@ -1,14 +1,47 @@
 #ifndef LUARIUM_GENERIC_H
 #define LUARIUM_GENERIC_H
 
+#include <iostream>
 #include <vector>
 #include <string>
+#include "glm/glm.hpp"
+
+struct Vertex {
+	// position
+	glm::vec3 Position;
+	// normal
+	glm::vec3 Normal;
+	// texCoords
+	glm::vec2 TexCoords;
+	// tangent
+	glm::vec3 Tangent;
+	// bitangent
+	glm::vec3 Bitangent;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+struct RenderMat {
+	glm::vec3 diffuse_color = glm::vec3(0,0,0);
+	glm::vec3 specular_color = glm::vec3(0,0,0);
+	glm::vec3 ambient_color = glm::vec3(0,0,0);
+
+	float shininess = 0;
+	float IOR = 0;
+	float opacity = 0;
+};
 
 namespace Luarium{
-	///Seperates a string into a vector of strings, separated by [seperator] (input string, seperator)
+	// Seperates a string into a vector of strings, separated by [seperator] (input string, seperator)
 	std::vector<std::string> segment(std::string &in, char seperator);
 
-	const float cubeVerts[108] = {
+	std::vector<Vertex> calcVertex(const std::vector<float> &verticies);
+
+	const std::vector<float> cubeVerts = {
 		// positions          
 		-1.0f,  1.0f, -1.0f,
 		-1.0f, -1.0f, -1.0f,
@@ -53,7 +86,7 @@ namespace Luarium{
 		1.0f, -1.0f,  1.0f
 	};
 
-	const int cubeIndices[36] = {
+	const std::vector<unsigned int> cubeIndices = {
 		0,  1,  2,      0,  2,  3,    // front
 		4,  5,  6,      4,  6,  7,    // back
 		8,  9,  10,     8,  10, 11,   // top
