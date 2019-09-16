69d6ae255d0e626f2f343e85fe0ed05862029c15
hippo_o_matic <hippo.o.matic@gmail.com>
2019-09-16 13:12:22 -0700

    Move relevant structs and functions to mesh.h

--- include/luarium/mesh.h
+++ include/luarium/mesh.h
@@ -14,7 +14,35 @@
 
 #include "luarium/shader.h"
 #include "luarium/object.h"
-#include "luarium/utility.h"
+
+struct Vertex {
+	// position
+	glm::vec3 pos;
+	// normal
+	glm::vec3 Normal;
+	// texCoords
+	glm::vec2 TexCoords;
+	// tangent
+	glm::vec3 Tangent;
+	// bitangent
+	glm::vec3 Bitangent;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+struct RenderMat {
+	glm::vec3 diffuse_color = glm::vec3(0,0,0);
+	glm::vec3 specular_color = glm::vec3(0,0,0);
+	glm::vec3 ambient_color = glm::vec3(0,0,0);
+
+	float shininess = 0;
+	float IOR = 0;
+	float opacity = 0;
+};
 
 /// A class for storing vertex, texture, and material data to be drawn as a 3d object
 class Mesh {
@@ -42,5 +70,9 @@ protected:
 	// initializes all the buffer objects/arrays
 	void setupMesh();
 };
+
+// Translates a basic array of verticies into a format readable by the Mesh class
+std::vector<Vertex> calcVertex(const std::vector<float> &verticies, const std::vector<float> &texcoords = {});
+
 #endif
 
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -107,3 +107,71 @@ void Mesh::setupMesh(){
 }
 
 
+std::vector<Vertex> calcVertex(const std::vector<float> &verticies, const std::vector<float> &texcoords) {
+	std::vector<Vertex> v;
+	
+	// Establish some empty Vertex structs
+	for(long unsigned int i=0; i < verticies.size() / 3; i++){
+		v.push_back(Vertex()); 
+	}
+
+	// Put all the vertex coords into the vertex vector
+	for(long unsigned int i = 0, j = 0; i <= v.size(); i++, j+=3) {
+		v[i].pos = glm::vec3(verticies[j], verticies[j+1], verticies[j+2]);
+	}
+
+	// Calculate vertex normals, 
+	for(long unsigned int i=0, j=0; i <= v.size(); i++, j+=3){ //The i + 3 is to make sure we don't go past where there are values
+		glm::vec3 U(v[j+1].pos - v[j].pos);
+		glm::vec3 V(v[j+2].pos - v[j].pos);
+
+		glm::vec3 N(
+			U.y*V.z - U.z*V.y,
+			U.z*V.x - U.x*V.z,
+			U.x*V.y - U.y*V.x
+		);
+ 
+		v[i].Normal = N;
+	}
+
+	// Generate texcoords if there are none
+	if (texcoords.empty()){
+		for(long unsigned int i=0; i + 4 <= v.size(); i+=4){
+			v[i].TexCoords = glm::vec2(0, 0);
+			v[i+1].TexCoords = glm::vec2(1, 0);
+			v[i+2].TexCoords = glm::vec2(1, 1);
+			v[i+3].TexCoords = glm::vec2(0, 1);
+		}
+	} else {
+		for(long unsigned int i=0; i < v.size(); i++){
+			v[i].TexCoords = glm::vec2(texcoords[i], texcoords[i+1]);
+		}
+	}
+
+	//Calculate the tangent and bitangents for each triangle
+	for(long unsigned int i=0; i + 2 < v.size(); i+=3){
+		glm::vec3 edge1 = v[i+1].pos - v[i].pos;
+		glm::vec3 edge2 = v[i+2].pos - v[i].pos;
+
+		glm::vec2 delta1 = v[i+1].TexCoords - v[i].TexCoords;
+		glm::vec2 delta2 = v[i+2].TexCoords - v[i].TexCoords;
+
+		// Scary math
+		float f = 1.0f / (delta1.x * delta2.y - delta2.x * delta1.y);
+
+		v[i].Tangent.x = f * (delta2.y * edge1.x - delta1.y * edge2.x);
+		v[i].Tangent.y = f * (delta2.y * edge1.y - delta1.y * edge2.y);
+		v[i].Tangent.z = f * (delta2.y * edge1.z - delta1.y * edge2.z);
+		v[i].Tangent = glm::normalize(v[i].Tangent);
+		v[i+1].Tangent = v[i].Tangent;
+		v[i+2].Tangent = v[i].Tangent;
+
+		v[i].Bitangent.x = f * (-delta2.x * edge1.x + delta1.x * edge2.x);
+		v[i].Bitangent.y = f * (-delta2.x * edge1.y + delta1.x * edge2.y);
+		v[i].Bitangent.z = f * (-delta2.x * edge1.z + delta1.x * edge2.z);
+		v[i].Bitangent = glm::normalize(v[i].Bitangent);
+		v[i+1].Bitangent = v[i].Bitangent;
+		v[i+2].Bitangent = v[i].Bitangent;
+	}
+	return v;
+}
