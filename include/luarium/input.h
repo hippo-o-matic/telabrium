7365bdb99d05d3cf98e8692028b0224fe254907f
hippo_o_matic <hippo.o.matic@gmail.com>
2019-09-30 14:29:23 -0700

    Add input system that can be configured with binds

--- include/luarium/input.h
+++ include/luarium/input.h
@@ -4,17 +4,64 @@
 #include <functional>
 
 #include "GLFW/glfw3.h"
+#include "json/json.h"
 
-namespace Input {
+#include "luarium/utility.h"
+
+// A class for setting up input binds, assigning keys to them, and polling those keys
+class Input {
+public:
     struct bind {
+        const char* name;
+        std::function<void()> func;
         int key;
-        int mode;
-        std::function<void(GLFWwindow *window)> func;
+        int action;
     };
 
-    extern std::vector<bind> binds;
+    Input(const char* path = "");
 
-    void addBind(int, std::function<void(GLFWwindow *window)>, int mode = GLFW_PRESS);
-    void removeBind(int);
+    /*  loadConfig(Json::Value& j) 
+        Loads a json configuration from (j).
+
+        The configuration file should be organized as such:
+        "keybinds" : [
+            ["bind_name", <int representing a GLFW_KEY]
+        ],
+        "mousebinds" : [
+            ["bind_name", ]
+        ]
+    */
+    void loadConfig(Json::Value& j);  
+    void loadConfigFile(const char* path);
+
+    void saveConfig(Json::Value&); // Outputs
+    void saveConfigFile(const char* path);
+
+    void addBind(const char* name, std::function<void()> func, int key = -2, int action = GLFW_PRESS); // Adds a keybind to run func() when pressed
+    void removeBind(const char* name); // Removes the bind (name)
+    void removeBind(int key); // Removes all binds from (key)
+    void editBindFunc(const char* name, std::function<void()> func);
+    void editBindKey(const char* name, int key, int action = -2);
+
+    void setMouseCallback(std::function<void(GLFWwindow*, double, double)>);
+    void setScrollCallback(std::function<void(GLFWwindow*, double, double)>);
+
     void process(GLFWwindow*);
-}
+
+    static void setActiveInput(Input*);
+
+    static void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods);
+    static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+    static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+    static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+private:
+    static double mouse_lastX;
+    static double mouse_lastY;
+
+    std::vector<bind> binds;
+    std::function<void(GLFWwindow*, double, double)> mouse_c;
+    std::function<void(GLFWwindow*, double, double)> scroll_c;
+
+    static Input* activeInput;
+}; 
--- src/input.cpp
+++ src/input.cpp
@@ -1,18 +1,141 @@
 #include "luarium/input.h"
 
-std::vector<Input::bind> Input::binds;
+double Input::mouse_lastX = 0;
+double Input::mouse_lastY = 0;
 
-void Input::addBind(int key, std::function<void(GLFWwindow *window)> func, int mode) {
-    binds.push_back({key, mode, func});
+Input* Input::activeInput = nullptr;
+
+
+Input::Input(const char* path) {
+    loadConfigFile(path);
+}
+
+void Input::loadConfigFile(const char* path) {
+
+}
+
+void Input::loadConfig(Json::Value& json) {
+    Json::Value b = json["keybinds"];
+
+    // Iterate through the array of keybind assignments
+    for(unsigned i = 0; i < b.size(); i++) {
+        std::string name = b[i][0].asString();
+        auto name_it = std::find_if(binds.begin(), binds.end(), [name](bind b){ return b.name == name; }); // Find the bind by name
+        if(name_it != binds.end())
+            binds[i].key = b[i][1].asInt(); // Set the key stored in json (b[i][1]) to the appropriate bind in the vector
+    }
+}
+
+void Input::saveConfig(Json::Value& json) {
+    for (unsigned i = 0; i < binds.size(); i++) {
+        json["keybinds"][i][0] = binds.at(i).name;
+        json["keybinds"][i][1] = binds.at(i).key;
+    }
+}
+
+
+void Input::addBind(const char* name, std::function<void()> func, int key, int action) {
+    if(key == GLFW_KEY_UNKNOWN)
+        LuariumLog("Can't add a bind to an unknown key", 2); return;
+
+    // If the bind doesn't already exist, push a new one to the binds vector
+    if(std::find_if(binds.begin(), binds.end(), [name](bind b){ return b.name == name; }) != binds.end()) { // Find the bind by name
+        binds.push_back({name, func, key, action,});
+    } else {
+        LuariumLog("A bind with the id \"" + std::string(name) + "\" already exists", 2);
+    }
+        
+}
+
+void Input::removeBind(const char* name) {
+    binds.erase(std::find_if(binds.begin(), binds.end(), [name](bind b){ return b.name == name; })); // Find the bind by name
 }
 
 void Input::removeBind(int key) {
-    binds.erase(std::find_if(binds.begin(), binds.end(), [key](bind b){ return b.key == key; }));
+    for(auto it = binds.begin(); it != binds.end(); it++) {
+        if(it->key == key)
+            binds.erase(it);
+    }
 }
 
-void Input::process(GLFWwindow* win) {
-    for (auto it = binds.begin(); it != binds.end(); it++) {
-        if (glfwGetKey(win, it->key) == it->mode)
-            it->func(win);
+
+void Input::editBindFunc(const char* name, std::function<void()> func) {
+    auto it = std::find_if(binds.begin(), binds.end(), [name](bind b){ return b.name == name; }); // Find the bind by name
+    if(it != binds.end()) {
+        it->func = func;
+    } else {
+        LuariumLog("Can't find bind \"" + std::string(name) + "\" to edit", 1);
     }
+}
+
+void Input::editBindKey(const char* name, int key, int action) {
+    auto it = std::find_if(binds.begin(), binds.end(), [name](bind b){ return b.name == name; }); // Find the bind by name
+    if(it != binds.end()) {
+        it->key = key; // Assign values if it does
+        it->action = (action == -2) ? it->action : action; // If the action is the default (-2), leave it as the same
+    } else {
+        LuariumLog("Can't find bind \"" + std::string(name) + "\" to edit", 1);
+    }
+}
+
+
+void setMouseCallback(std::function<void(GLFWwindow*, double, double)>) {
+
+}
+
+void setScrollCallback(std::function<void(GLFWwindow*, double, double)>) {
+
+}
+
+
+
+void Input::process(GLFWwindow* win) {
+    for(auto it = binds.begin(); it != binds.end(); it++) {
+        if (glfwGetKey(win, it->key) == it->action)
+            it->func();
+    }
+}
+
+void Input::setActiveInput(Input* in) {
+    activeInput = in;
+}
+
+
+void Input::key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) {
+    for(auto it = activeInput->binds.begin(); it != activeInput->binds.end(); it++) {
+        if(it->key == key && it->action == action)
+            it->func();
+    }
+}
+
+void Input::mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+    if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_NORMAL) {
+		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	}
+
+    for(auto it = activeInput->binds.begin(); it != activeInput->binds.end(); it++) {
+        if(it->key == button && it->action == action)
+            it->func();
+    }
+}
+
+void Input::mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    // if (firstMouse)	{
+	// 	lastX = xpos;
+	// 	lastY = ypos;
+	// 	firstMouse = false;
+	// }
+
+	float xoffset = xpos - mouse_lastX;
+	float yoffset = mouse_lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	mouse_lastX = xpos;
+	mouse_lastY = ypos;
+
+    activeInput->mouse_c(window, xoffset, yoffset);
+}
+
+void Input::scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+
 }
